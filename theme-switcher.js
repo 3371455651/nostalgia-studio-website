c11a8a065de87658e7db7397d390feab5b0d4a96
lenmei233 <106967003+3371455651@users.noreply.github.com>
2024-07-11 14:41:13 +0800

    Add files via upload

---
+++ theme-switcher.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var themeSwitcher = document.getElementById('theme-switcher');
+    themeSwitcher.addEventListener('click', function() {
+        document.body.classList.toggle('dark-theme');
+    });
+});
